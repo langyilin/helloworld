8b76f16a9027dadf778e08dee2ca570b590ad580
nero <416739124@qq.com>
2018-07-20 17:06:54 +0800

    static resource

--- src/main/java/com/cloudtogo/demo/HelloworldApplication.java
+++ src/main/java/com/cloudtogo/demo/HelloworldApplication.java
@@ -4,19 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
 public class HelloworldApplication {
 
 
-	@RequestMapping("")
-	@ResponseBody
-	public String helloWorld() {
-		return "Hello World!";
-	}
 
+	@RequestMapping("")
+	public String helloWorld() {
+		return "helloworld";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
